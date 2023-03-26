ec6cf0e384f4394ac0cc458100f72e3c022ddfc1
Andrew J Wippler <andrew.wippler@gmail.com>
2023-03-26 16:46:01 +0300

    Fix delete tag

--- api/database/migrations/1679411582123_illustrations_tags.ts
+++ api/database/migrations/1679411582123_illustrations_tags.ts
@@ -4,10 +4,11 @@ export default class extends BaseSchema {
   protected tableName = 'illustration_tag'
 
   public async up () {
+    this.schema.dropTable('ill_tags')
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('tag_id').unsigned().references('tags.id')
-      table.integer('illustration_id').unsigned().references('illustrations.id')
+      table.integer('tag_id').unsigned().references('tags.id').onDelete('CASCADE')
+      table.integer('illustration_id').unsigned().references('illustrations.id').onDelete('CASCADE')
       table.unique(['tag_id', 'illustration_id'])
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
