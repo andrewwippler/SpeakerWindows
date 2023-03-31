504436a86fcbf4864ac9edacd267cbea6991be23
Andrew J Wippler <andrew.wippler@gmail.com>
2023-03-31 09:29:58 +0300

    Fix #45

--- frontend/src/pages/illustration/[id].tsx
+++ frontend/src/pages/illustration/[id].tsx
@@ -157,14 +157,14 @@ export default function IllustrationWrapper() {
       <div className="text-xl font-bold pt-8 text-sky-900">
           <span className='mr-4'>Illustration Use:</span>
         </div>
-        <div className='grid grid-cols-3 text-sky-500'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-sky-500'>
           {illustration && illustration?.places.length > 0 ? illustration.places.map((p, index) => (
 
             <div key={index} className='w-full gap-2 pb-2'>
               <span className='mr-2'>{p.place}, {p.location} - {p.used}</span>
               <button
                 onClick={() => handleDeletePlace(p)}
-                className='rounded-md p-2 font-semibold text-sm bg-red-300 hover:bg-red-500 text-white shadow-sm inline-flex items-center'>
+                className='hidden rounded-md p-2 font-semibold text-sm bg-red-300 hover:bg-red-500 text-white shadow-sm lg:inline-flex items-center'>
             <TrashIcon className="h-4 w-4" /></button>
             </div>
             ))
@@ -174,7 +174,7 @@ export default function IllustrationWrapper() {
           </div>
             <form className='mt-4' onSubmit={handlePlaceAdd}>
           <div className="grid grid-cols-6">
-          <div className="col-span-2">
+          <div className="mb-1 col-span-6 md:col-span-2">
             <label htmlFor="Place" className="sr-only block text-sm font-medium leading-6 text-gray-900">
               Place
             </label>
@@ -183,10 +183,10 @@ export default function IllustrationWrapper() {
               name="Place"
               id="Place"
               placeholder="Place"
-              className="block w-full rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full lg:rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
-            <div className="col-span-2">
+            <div className="mb-1 col-span-6 md:col-span-2">
               <label htmlFor="Location" className="sr-only block text-sm font-medium leading-6 text-gray-900">
                 Location
               </label>
@@ -198,7 +198,7 @@ export default function IllustrationWrapper() {
                 className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
             </div>
-            <div className="col-span-1">
+            <div className="mb-1 col-span-6 md:col-span-1">
               <label htmlFor="Used" className="sr-only block text-sm font-medium leading-6 text-gray-900">
                 Used
               </label>
@@ -211,7 +211,7 @@ export default function IllustrationWrapper() {
                 />
             </div>
             <button type='submit'
-              className="inline-flex justify-center rounded-r-md bg-indigo-300 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+              className="mb-1 col-span-6 md:col-span-1 inline-flex justify-center lg:rounded-r-md bg-indigo-300 px-2 lg:px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
               <PlusIcon className='h-5 w-5 mr-2'/>
               Add Place</button>
         </div>
