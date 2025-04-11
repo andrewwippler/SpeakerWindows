b526b7e27b39bee32b8381f5d5d73b1b5c20f418
Andrew J Wippler <andrew.wippler@gmail.com>
2025-04-11 08:46:40 +0300

    erase typesense for now

--- api/app/controllers/http/SearchesController.ts
+++ api/app/controllers/http/SearchesController.ts
@@ -14,19 +14,21 @@ export default class SearchesController {
       return response.noContent()
     }
 
-    const illustrations = await Illustration.search(search)
-      .where('user_id', `${auth.user?.id}`).get()
-      // ((query) => {
-      //   query.where('title', search)
-      //     .orWhere('content', 'LIKE', `%${search}%`)
-      //     .orWhere('author', 'LIKE', `%${search}%`)
-      // })
-      // .andWhere
+    // const illustrations = await Illustration.search(search)
+    //   .where('user_id', auth.user?.id).get()
+
+    const illustrations = await Illustration.query((query) => {
+        query.where('title', search)
+          .orWhere('content', 'LIKE', `%${search}%`)
+          .orWhere('author', 'LIKE', `%${search}%`)
+      })
+      .andWhere('user_id', `${auth.user?.id}`)
     const tagSanitizedSearch = _.startCase(search).replace(/ /g, '-')
-    const tags = await Tag.search(tagSanitizedSearch).where('user_id', `${auth.user?.id}`).get()
+    const tags = await Tag.query().where('name', tagSanitizedSearch).andWhere('user_id', `${auth.user?.id}`)
+    // const tags = await Tag.search(tagSanitizedSearch).where('user_id', `${auth.user?.id}`).get()
     const places = await Place.query().preload('illustration').where('place',search).andWhere('user_id', `${auth.user?.id}`)
 
-    // console.log({ message: 'success',user_id: `${auth.user?.id}`, searchString: search, data: { illustrations, places, tags } })
+    // console.log({ message: 'success',user_id: `${auth.user?.id}`, searchString: search, tagSanitizedSearch, data: { illustrations, places, tags } })
 
     return response.send({ message: 'success', searchString: search, data: { illustrations, places, tags } })
   }
