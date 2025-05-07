e7a6ab35714a7f4fd4a3b842e3d7bf4810b38e5c
Andrew J Wippler <andrew.wippler@gmail.com>
2025-05-07 08:40:27 +0300

    add testing seed to docker-compose file, add update author

---
+++ api/tests/functional/author.spec.ts
@@ -0,0 +1,94 @@
+import { test } from '@japa/runner'
+import db from '@adonisjs/lucid/services/db'
+import IllustrationFactory from '#database/factories/IllustrationFactory'
+import UserFactory from '#database/factories/UserFactory'
+import PlaceFactory from '#database/factories/PlaceFactory'
+import Illustration from '#models/illustration'
+import TagFactory from '#database/factories/TagFactory'
+import Tag from '#models/tag'
+import Place from '#models/place'
+import User from '#models/user'
+import { ModelObject } from '@adonisjs/lucid/types/model'
+let goodUser: User, badUser: User, testTagIdOne: string | number | ModelObject, testTagIdTwo: number, illustration
+
+test.group('Authors', (group) => {
+  group.each.setup(async () => {
+    await db.beginGlobalTransaction()
+    return () => db.rollbackGlobalTransaction()
+  })
+  group.setup(async () => {
+
+    goodUser = await UserFactory.merge({password: 'oasssadfasdf'}).create()
+    badUser = await UserFactory.merge({password: 'oasssadfasdf'}).create() // bad user does not have access to good user
+
+
+  })
+
+  group.teardown(async () => {
+    await goodUser.delete()
+    await badUser.delete()
+    await Illustration.query().delete()
+    await Tag.query().delete()
+    await Place.query().delete()
+  })
+  test('Author routes', async ({ client, assert }) => {
+    const loggedInUser = await client.post('/login').json({ email: goodUser.email, password: 'oasssadfasdf' })
+    const secondLoggedInUser = await client.post('/login').json({ email: badUser.email, password: 'oasssadfasdf' })
+
+    const illustration =  {
+      author: 'testy mctest',
+      title: 'New Post',
+      source: 'test',
+      content: 'this shall pass as new',
+    }
+    await client.post('/illustration').bearerToken(loggedInUser.body().token).json(illustration)
+
+    const second =  {
+      author: 'test2',
+      title: 'New Post',
+      source: 'test',
+      content: 'this shall pass as new',
+    }
+    await client.post('/illustration').bearerToken(loggedInUser.body().token).json(second)
+    await client.post('/illustration').bearerToken(secondLoggedInUser.body().token).json(second)
+
+    const both = await client.get('/illustration/authors').bearerToken(loggedInUser.body().token)
+    const one = await client.get('/illustration/authors').bearerToken(secondLoggedInUser.body().token)
+
+    both.assertStatus(200)
+    assert.equal(both.body().length, 3)
+
+    one.assertStatus(200)
+    assert.equal(one.body().length, 1)
+
+
+    const response = await client.get('/author/'+illustration.author).bearerToken(loggedInUser.body().token)
+    response.assertStatus(200)
+    assert.equal(response.body().length, 1)
+    const last = await client.get('/author/boogers').bearerToken(loggedInUser.body().token)
+    last.assertStatus(204)
+
+  })
+
+  test('Update author', async ({ client, assert }) => {
+    const loggedInUser = await client.post('/login').json({ email: goodUser.email, password: 'oasssadfasdf' })
+
+    const illustration =  {
+      author: 'testy mctest',
+      title: 'New Post',
+      source: 'test',
+      content: 'this shall pass as new',
+    }
+    await client.post('/illustration').bearerToken(loggedInUser.body().token).json(illustration)
+
+    const response = await client.put('/author/' + illustration.author).bearerToken(loggedInUser.body().token).json({ author: 'new author' })
+    response.assertStatus(200)
+    assert.equal(response.body().message, 'Author updated from testy mctest to new author')
+    const updated = await client.get('/author/new author').bearerToken(loggedInUser.body().token)
+    updated.assertStatus(200)
+    assert.equal(updated.body().length, 1)
+    const last = await client.get('/author/boogers').bearerToken(loggedInUser.body().token)
+    last.assertStatus(204)
+  })
+
+})
--- frontend/src/pages/author/[id].tsx
+++ frontend/src/pages/author/[id].tsx
@@ -9,7 +9,12 @@ import Layout from '@/components/Layout';
 import Head from 'next/head';
 import { setRedirect } from '@/features/ui/reducer';
 import { useAppSelector, useAppDispatch } from '@/hooks'
-
+import {
+  PencilSquareIcon,
+  CheckCircleIcon,
+  TrashIcon,
+} from "@heroicons/react/24/solid";
+import { setFlashMessage } from "@/features/flash/reducer";
 
 export default function Author() {
   const router = useRouter()
@@ -22,6 +27,8 @@ export default function Author() {
 
   const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(false)
+
+  const [editAuthor, setEditAuthor] = useState(false);
 
   useEffect(() => {
     if (!user?.token) dispatch(setRedirect(`/author/${name}`))
@@ -38,6 +45,29 @@ export default function Author() {
     });
   }, [name, dispatch, user?.token]);
 
+    const handleSave = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const newname = event.currentTarget.Author.value.trim();
+
+      // update author
+      api.put(`/author/${name}`, { name: newname }, user?.token).then((data) => {
+        if (data.message != "Updated successfully") {
+          dispatch(
+            setFlashMessage({ severity: "danger", message: data.message })
+          );
+          return;
+        }
+        dispatch(
+          setFlashMessage({
+            severity: "success",
+            message: `Author "${name}" was renamed to ${newname}.`,
+          })
+        );
+        setEditAuthor(false);
+        router.replace(`/author/${newname}`);
+      });
+    };
+
 
   if (!user?.token) return
   if (isLoading) return (
@@ -53,7 +83,44 @@ export default function Author() {
       </Head>
       <div className="text-xl font-bold pb-4 text-sky-900">
 
-        <span className='mr-4'>{name}</span>
+        <span className='mr-4'>
+        {editAuthor ? (
+          <form className="mt-8 space-y-6" onSubmit={handleSave}>
+            <label htmlFor="Author" className="sr-only">
+              Rename Author
+            </label>
+            <input
+              id="Author"
+              name="Author"
+              type="Author"
+              autoComplete="Author"
+              required
+              className="pl-1.5 py-1.5 ring-1 mr-4"
+              placeholder="Author"
+              defaultValue={name}
+            />
+
+            <button
+              type="submit"
+              className="px-4 py-2 mr-4 mt-2 font-semibold text-sm bg-green-300 hover:bg-green-500 text-white rounded-full shadow-sm inline-flex items-center"
+            >
+              <CheckCircleIcon className="h-4 w-4 mr-2" />
+              Update Author
+            </button>
+          </form>
+        ) : (
+          <>
+            <span className="mr-4">{name}</span>
+            <button
+              onClick={() => setEditAuthor(true)}
+              className="px-4 py-2 mr-4 mt-2 font-semibold text-sm bg-green-300 hover:bg-green-500 text-white rounded-full shadow-sm inline-flex items-center"
+            >
+              <PencilSquareIcon className="h-4 w-4 mr-2" />
+              Edit Author
+            </button>
+          </>
+        )}
+        </span>
 
       </div>
 
