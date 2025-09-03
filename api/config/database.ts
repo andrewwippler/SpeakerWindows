2e7736b0ba30569a2cd8765acb41d39b447170cd
Andrew J Wippler <andrew.wippler@gmail.com>
2025-09-03 15:14:00 +0300

    bugfix and add pgsql

--- api/config/database.ts
+++ api/config/database.ts
@@ -79,6 +79,23 @@ const databaseConfig = defineConfig({
       },
     },
 
+
+    pg: {
+      client: 'pg',
+      connection: {
+        host: env.get('DB_HOST'),
+        port: env.get('DB_PORT'),
+        user: env.get('DB_USER'),
+        password: env.get('DB_PASSWORD', ''),
+        database: env.get('DB_DATABASE'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      debug: false,
+    }
+
+
   }
 })
 
