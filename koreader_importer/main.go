ed66eedb713e715b52e1a7f6f0eae7153916fd29
Andrew J Wippler <andrew.wippler@gmail.com>
2026-01-06 14:29:25 +0200

    hotfix - allow export all and export one book highlights

--- koreader_importer/main.go
+++ koreader_importer/main.go
@@ -43,115 +43,51 @@ func parseKoreaderFile(path string) ([]Illustration, error) {
 		return nil, err
 	}
 
-	var out []Illustration
-
-	// try as array of objects
-	var arr []map[string]interface{}
-	if err := json.Unmarshal(data, &arr); err == nil {
-		for _, item := range arr {
-			content := pickString(item, []string{"text", "highlight", "content", "annotation"})
-			content = tidyText(content)
-			if content == "" {
-				// maybe nested under "highlights"
-				if h, ok := item["highlights"].([]interface{}); ok {
-					for _, hh := range h {
-						if hm, ok := hh.(map[string]interface{}); ok {
-							c := pickString(hm, []string{"text", "highlight", "content", "annotation"})
-							c = tidyText(c)
-							if c != "" {
-								// Title should be first 100 chars of the highlight
-								t := c
-								if len(t) > 100 {
-									t = t[:100]
-								}
-								author := pickString(item, []string{"author", "bookAuthor", "book_author"})
-								// Construct source
-								bookTitle := pickString(item, []string{"title", "bookTitle", "book_title", "book"})
-								locationType := pickString(item, []string{"locationType", "location_type"})
-								location := pickString(item, []string{"location", "page", "position"})
-								source := fmt.Sprintf("%s %s %s", bookTitle, locationType, location)
-
-								// Construct tags
-								color := pickString(item, []string{"color", "highlightColor"})
-								tags := []string{color, "To Fix"}
-								if len(c) < 150 {
-									tags = append(tags, "Quotes")
-								}
-
-								out = append(out, Illustration{Title: t, Author: author, Source: source, Content: c, Tags: tags})
-							}
-						}
-					}
-					continue
-				}
-				continue
-			}
-			// Title should be first 100 chars of the highlight
-			title := content
-			if len(title) > 100 {
-				title = title[:100]
-			}
-			author := pickString(item, []string{"author", "bookAuthor", "book_author"})
-			// Construct source
-			bookTitle := pickString(item, []string{"title", "bookTitle", "book_title", "file", "book"})
-			locationType := pickString(item, []string{"locationType", "location_type"})
-			location := pickString(item, []string{"location", "page", "position"})
-			source := fmt.Sprintf("%s %s %s", bookTitle, locationType, location)
-			// Construct tags
-			amazonID := pickString(item, []string{"amazonID", "amazon_book_id", "amazon"})
-			color := pickString(item, []string{"color", "highlightColor"})
-			tags := []string{amazonID, color, "To Fix"}
-			if len(content) < 150 {
-				tags = append(tags, "Quotes")
-			}
-			out = append(out, Illustration{Title: title, Author: author, Source: source, Content: content, Tags: tags})
-		}
-		return out, nil
-	}
-
-	// try as top-level object with books
-	var obj map[string]interface{}
-	if err := json.Unmarshal(data, &obj); err != nil {
+	var root interface{}
+	if err := json.Unmarshal(data, &root); err != nil {
 		return nil, err
 	}
 
-	// iterate values and look for highlights arrays
-	for _, v := range obj {
-		switch t := v.(type) {
+	var out []Illustration
+
+	// recursive collector: walk the JSON tree and extract any object that looks like a highlight
+	var collect func(interface{})
+	collect = func(node interface{}) {
+		switch v := node.(type) {
 		case []interface{}:
-			for _, e := range t {
-				if em, ok := e.(map[string]interface{}); ok {
-					content := pickString(em, []string{"text", "highlight", "content", "annotation"})
-					content = tidyText(content)
-					if content == "" {
-						continue
-					}
-					// Title should be first 100 chars of the highlight
-					title := content
-					if len(title) > 100 {
-						title = title[:100]
-					}
-					author := pickString(em, []string{"author", "bookAuthor", "book_author"})
-					// Construct source
-					bookTitle := pickString(em, []string{"title", "bookTitle", "book_title", "file", "book"})
-					locationType := pickString(em, []string{"locationType", "location_type"})
-					location := pickString(em, []string{"location", "page", "position"})
-					source := fmt.Sprintf("%s %s %s", bookTitle, locationType, location)
-					// Construct tags
-					amazonID := pickString(em, []string{"amazonID", "amazon_book_id", "amazon"})
-					color := pickString(em, []string{"color", "highlightColor"})
-					tags := []string{amazonID, color, "To Fix"}
-					if len(content) < 150 {
-						tags = append(tags, "Quotes")
-					}
-					out = append(out, Illustration{Title: title, Author: author, Source: source, Content: content, Tags: tags})
-				}
+			for _, e := range v {
+				collect(e)
 			}
 		case map[string]interface{}:
-			// maybe a book object with highlights
-			if h, ok := t["highlights"].([]interface{}); ok {
-				title := pickString(t, []string{"title", "bookTitle", "book_title", "file"})
-				author := pickString(t, []string{"author", "bookAuthor", "book_author"})
+			// If this map itself contains highlight text, treat it as a highlight
+			content := pickString(v, []string{"text", "highlight", "content", "annotation"})
+			content = tidyText(content)
+			if content != "" {
+				title := content
+				if len(title) > 100 {
+					title = title[:100]
+				}
+				author := pickString(v, []string{"author", "bookAuthor", "book_author"})
+				bookTitle := pickString(v, []string{"title", "bookTitle", "book_title", "file", "book"})
+				locationType := pickString(v, []string{"locationType", "location_type"})
+				location := pickString(v, []string{"location", "page", "position"})
+				source := fmt.Sprintf("%s %s %s", bookTitle, locationType, location)
+
+				amazonID := pickString(v, []string{"amazonID", "amazon_book_id", "amazon"})
+				color := pickString(v, []string{"color", "highlightColor"})
+				tags := []string{amazonID, color, "To Fix"}
+				if len(content) < 150 {
+					tags = append(tags, "Quotes")
+				}
+				out = append(out, Illustration{Title: title, Author: author, Source: source, Content: content, Tags: tags})
+				// continue traversal to find nested highlights as well
+			}
+
+			// If this map has a "highlights" array, process those specifically
+			if h, ok := v["highlights"].([]interface{}); ok {
+				// book-level metadata
+				bookTitle := pickString(v, []string{"title", "bookTitle", "book_title", "file", "book"})
+				author := pickString(v, []string{"author", "bookAuthor", "book_author"})
 				for _, hh := range h {
 					if hm, ok := hh.(map[string]interface{}); ok {
 						content := pickString(hm, []string{"text", "highlight", "content", "annotation"})
@@ -159,35 +95,32 @@ func parseKoreaderFile(path string) ([]Illustration, error) {
 						if content == "" {
 							continue
 						}
-						// Title should be first 100 chars of the highlight
 						t := content
 						if len(t) > 100 {
 							t = t[:100]
 						}
-						source := title
-						// Construct tags
-						amazonID := ""
-						color := ""
-						if hmVal, ok := hm["amazonID"]; ok {
-							if s, ok2 := hmVal.(string); ok2 {
-								amazonID = tidyText(s)
-							}
-						}
-						if hmVal, ok := hm["color"]; ok {
-							if s, ok2 := hmVal.(string); ok2 {
-								color = tidyText(s)
-							}
-						}
-						tags := []string{amazonID, color, "To Fix"}
+						source := bookTitle
+						color := pickString(hm, []string{"color", "highlightColor"})
+						tags := []string{color, "To Fix"}
 						if len(content) < 150 {
 							tags = append(tags, "Quotes")
 						}
 						out = append(out, Illustration{Title: t, Author: author, Source: source, Content: content, Tags: tags})
+					} else {
+						// recurse in case highlights array contains nested structures
+						collect(hh)
 					}
 				}
 			}
+
+			// recurse into child values
+			for _, child := range v {
+				collect(child)
+			}
 		}
 	}
+
+	collect(root)
 
 	return out, nil
 }
